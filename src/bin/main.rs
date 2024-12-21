1455821de0c7c94a439e0ef3dcc946cbea667e2b
EduardoStarZ <pathver@gmail.com>
2024-12-21 09:11:25 -0300

    made the functionality to read files into the command line arguments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,12 @@
-use os::{get_file, scanner::{Scanner, Scanning}};
+use star_lang::{get_file, scanner::{Scanner, Scanning}};
+use std::env::args;
 
 fn main() {
-    let file_content : String = get_file("test.txt").to_string();
+    let raw_args = args().into_iter().collect::<Vec<String>>(); 
+
+    let file : &String = raw_args.get(1).unwrap();
+
+    let file_content : String = get_file(file.as_str()).to_string();
     let mut scanner : Scanner = Scanner {file: file_content.clone(), char_idx: 0, curr_char: '\n', file_size: file_content.clone().chars().count() };
 
     scanner.scan_file();
