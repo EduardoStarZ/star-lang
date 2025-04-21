dfeb886c7eae31b28625f4feac7f7df4b247f728
EduardoStarZ <pathver@gmail.com>
2025-04-21 18:08:55 -0300

    probably a better way to make an AST, will implement later

--- src/tree.rs
+++ src/tree.rs
@@ -1,38 +1,24 @@
 use crate::tokens::TokenLiteral;
 
-pub struct Node<'a> {
-    pub value : TokenLiteral,
-    pub left : Option<&'a Node<'a >>,
-    pub right: Option<&'a Node<'a>>
+pub enum Operator {
+    Add,
+    Subtract,
+    Divide,
+    Multiply,
+    Module
 }
 
-pub trait NextNode<'a> {
-    fn has_next_node(&self) -> bool;
-    fn is_valid_node(&self) -> bool;
-    fn new(token : TokenLiteral) -> Node<'a>;
+pub enum Node {
+     Int(isize),
+     UnaryExpr {
+        value : isize,
+        operator : Operator
+     },
+     BinaryExpr {
+        operator: Operator,
+        left_node : Box<Node>,
+        right_node : Box<Node>
+     }
 }
 
-impl<'a> NextNode<'a> for Node<'a> {
-    fn has_next_node(&self) -> bool {
-        return match self.right {
-            Some(_) => true,
-            None => false
-        };
-    }
 
-    fn is_valid_node(&self) -> bool {
-        if self.left.is_some() && self.right.is_some() {
-            return true;
-        }
-        return false;
-    }
-
-    fn new(token : TokenLiteral) -> Node<'a> {
-        return Node{
-            value: token,
-            right: None,
-            left: None
-        }; 
-    }
-        
-}
